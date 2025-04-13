6a84eb7aadbef41c4ca051bea1fe88a3c939d2b3
spttmssla55 <spttmssla555@naver.com>
2025-04-14 01:07:17 +0900

    User- 계정 추가

--- begin/src/MyPage/User.js
+++ begin/src/MyPage/User.js
@@ -6,6 +6,41 @@ const User = () => {
   const [selectedMenu, setSelectedMenu] = useState('info'); // Default to 'info' menu
   const fileInputRef = useRef(null);
 
+  // 이름 변경
+  const [name, setName] = useState('이름을 입력해주세요');
+  const [age, setAge] = useState('나이를 입력해주세요');
+  const [email, setEmail] = useState('이메일을 입력해주세요');
+  const [isEditing, setIsEditing] = useState(false);
+  const [editData, setEditData] = useState({ name: '', age: '', email: '' });
+  const [phone, setPhone] = useState("전화번호를 입력해주세요");
+
+  const handleEditClick = () => {
+    setIsEditing(true);
+    setEditData({ name, age, email, phone }); // 편집 모드일 때 현재 값들을 입력 필드에 보여주기 위함
+  };
+
+  const handleSaveClick = () => {
+    setName(editData.name);
+    setAge(editData.age);
+    setEmail(editData.email);
+    setPhone(editData.phone);
+    setIsEditing(false);
+  };
+
+  const handleCancelClick = () => {
+    setIsEditing(false);
+    setEditData({ name, age, email, phone }); // 편집 취소 시 이전 값으로 복원 (선택 사항)
+  };
+
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setEditData(prevData => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+  // 끝
+
   const handleMenuClick = (menu) => {
     setSelectedMenu(menu);
   };
@@ -29,17 +64,172 @@ const User = () => {
     switch (selectedMenu) {
       case 'info':
         return (
-          <div>
-            <button
+          <div style={{ display: 'flex', flexDirection: 'column' }}>
+            <div style={{ display: 'flex', justifyContent: 'flex-end', marginBottom: '10px' }}>
+              <button
+                style={{
+                  padding: '10px',
+                  fontSize: '15px',
+                  marginRight: '5px',
+                  background: "white"
+                }}
+                onClick={isEditing ? handleSaveClick : handleEditClick}
+              >
+                {isEditing ? '저장' : '편집'}
+              </button>
+              {isEditing && (
+                <button
+                  style={{
+                    padding: '5px',
+                    fontSize: '15px',
+                  }}
+                  onClick={handleCancelClick}
+                >
+                  취소
+                </button>
+              )}
+            </div>
+            <div
               style={{
-                float: "right",
-              }}>편집</button>
-            <p>이름: 홍길동</p>
-            <p>나이: 30</p>
-            <p>이메일: gildong.hong@example.com</p>
+                display: 'flex',
+                flexDirection: 'row',
+                marginBottom: '10px',
+                fontSize: '20px',
+                alignItems: 'center',
+              }}
+            >
+              <p style={{ fontWeight: 'bold', marginRight: '10px' }}>이름:</p>
+              {isEditing ? (
+                <input
+                  type="text"
+                  name="name"
+                  value={editData.name}
+                  onChange={handleInputChange}
+                  style={{ fontSize: '20px' }}
+                />
+              ) : (
+                <p style={{ fontSize: '20px', marginLeft: "250px" }}>{name}</p>
+              )}
+            </div>
+            <div
+              style={{
+                marginTop: '100px',
+                display: 'flex',
+                flexDirection: 'row',
+                marginBottom: '10px',
+                fontSize: '20px',
+                alignItems: 'center',
+              }}
+            >
+              <p style={{ fontWeight: 'bold', marginRight: '10px' }}>나이:</p>
+              {isEditing ? (
+                <input
+                  type="text"
+                  name="age"
+                  value={editData.age}
+                  onChange={handleInputChange}
+                  style={{ fontSize: '20px' }}
+                />
+              ) : (
+                <p style={{ fontSize: '20px', marginLeft: "250px" }}>{age}</p>
+              )}
+            </div>
+            <div
+              style={{
+                marginTop: '100px',
+                display: 'flex',
+                flexDirection: 'row',
+                marginBottom: '20px',
+                fontSize: '20px',
+                alignItems: 'center',
+              }}
+            >
+              <p style={{ fontWeight: 'bold', marginRight: '10px' }}>이메일:</p>
+              {isEditing ? (
+                <input
+                  type="email"
+                  name="email"
+                  value={editData.email}
+                  onChange={handleInputChange}
+                  style={{ fontSize: '20px' }}
+                />
+              ) : (
+                <p style={{ fontSize: '20px', marginLeft: "250px" }}>{email}</p>
+              )}
+            </div>
+            <div
+              style={{
+                marginTop: '100px',
+                display: 'flex',
+                flexDirection: 'row',
+                marginBottom: '20px',
+                fontSize: '20px',
+                alignItems: 'center',
+              }}
+            >
+              <p style={{ fontWeight: 'bold', marginRight: '10px' }}>전화번호:</p>
+              {isEditing ? (
+                <input
+                  type="phone"
+                  name="phone"
+                  value={editData.phone}
+                  onChange={handleInputChange}
+                  style={{ fontSize: '20px' }}
+                />
+              ) : (
+                <p style={{ fontSize: '20px', marginLeft: "250px" }}>{phone}</p>
+              )}
+            </div>
+            <div
+              style={{
+                marginTop: '100px',
+                display: 'flex',
+                flexDirection: 'row',
+                marginBottom: '20px',
+                fontSize: '20px',
+                alignItems: 'center',
+              }}
+            >
+              <p style={{ fontWeight: 'bold', marginRight: '10px' }}>연동:</p>
+              {isEditing ? (
+                <input
+                  type="email"
+                  name="email"
+                  value={editData.email}
+                  onChange={handleInputChange}
+                  style={{ fontSize: '20px' }}
+                />
+              ) : (
+                <p style={{ fontSize: '20px', marginLeft: "250px" }}>{email}</p>
+              )}
+            </div>
+            <div
+              style={{
+                marginTop: '100px',
+                display: 'flex',
+                flexDirection: 'row',
+                marginBottom: '20px',
+                fontSize: '20px',
+                alignItems: 'center',
+              }}
+            >
+              <p style={{ fontWeight: 'bold', marginRight: '10px' }}>수신동의:</p>
+              {isEditing ? (
+                <input
+                  type="email"
+                  name="email"
+                  value={editData.email}
+                  onChange={handleInputChange}
+                  style={{ fontSize: '20px' }}
+                />
+              ) : (
+                <p style={{ fontSize: '20px', marginLeft: "250px" }}>{email}</p>
+              )}
+            </div>
             {/* Add more personal information here */}
           </div>
         );
+
       case 'image':
         return (
           <div>
@@ -55,9 +245,9 @@ const User = () => {
             )}
           </div>
         );
-        case 'wishlist':
-          return(
-            <div>
+      case 'wishlist':
+        return (
+          <div>
             <h2>위시리스트</h2>
             {selectedImage ? (
               <img
@@ -69,22 +259,22 @@ const User = () => {
               <p>선택된 이미지가 없습니다.</p>
             )}
           </div>
-          );
-          case 'recently':
-            return(
-              <div>
-              <h2>최근 본</h2>
-              {selectedImage ? (
-                <img
-                  src={selectedImage}
-                  alt="Selected Image"
-                  style={{ maxWidth: '100%', maxHeight: '300px' }}
-                />
-              ) : (
-                <p>선택된 이미지가 없습니다.</p>
-              )}
-            </div>
-            );
+        );
+      case 'recently':
+        return (
+          <div>
+            <h2>최근 본</h2>
+            {selectedImage ? (
+              <img
+                src={selectedImage}
+                alt="Selected Image"
+                style={{ maxWidth: '100%', maxHeight: '300px' }}
+              />
+            ) : (
+              <p>선택된 이미지가 없습니다.</p>
+            )}
+          </div>
+        );
       default:
         return <div>선택된 메뉴가 없습니다.</div>;
     }
@@ -168,7 +358,7 @@ const User = () => {
               border: '1px solid gray',
               padding: '20px',
               textAlign: 'center',
-              height: 'auto',
+              height: '580px',
               display: 'flex',
               flexDirection: 'column',
               gap: '10px',
@@ -176,6 +366,8 @@ const User = () => {
           >
             <button
               style={{
+                fontSize: '20px',
+                fontWeight: 'bold',
                 padding: '10px',
                 border: 'none',
                 cursor: 'pointer',
@@ -187,6 +379,8 @@ const User = () => {
             </button>
             <button
               style={{
+                fontSize: '20px',
+                fontWeight: 'bold',
                 padding: '10px',
                 border: 'none',
                 cursor: 'pointer',
@@ -198,6 +392,8 @@ const User = () => {
             </button>
             <button
               style={{
+                fontSize: '20px',
+                fontWeight: 'bold',
                 padding: '10px',
                 border: 'none',
                 cursor: 'pointer',
@@ -209,6 +405,8 @@ const User = () => {
             </button>
             <button
               style={{
+                fontSize: '20px',
+                fontWeight: 'bold',
                 padding: '10px',
                 border: 'none',
                 cursor: 'pointer',
